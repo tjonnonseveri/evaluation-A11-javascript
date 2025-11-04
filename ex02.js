ba805369f9c8e1aecaf742ecb84e97e799e8e160
Théo <tjonnonseveri@edenschool.fr>
2025-11-04 16:03:01 +0100

    ex02

--- ex02.js
+++ ex02.js
@@ -1,5 +1,5 @@
 function greetings(firstname) {
-    return "Bienvenue, " + firstname + "!";
+	return "Bienvenue, " + firstname + " !";
 }
 
 console.log(greetings("")); // Résultat attendu: "Bienvenue, !"
